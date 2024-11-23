c755d70c4d72cdedd91e207ddc31b709cb9c8d03
Jangwon0319 <ryujw0319@gmail.com>
2024-11-24 04:45:46 +0900

    feat : diary 구현 중

--- src/main/java/com/dteam/momentree/application/config/auth/Auth.java
+++ src/main/java/com/dteam/momentree/application/config/auth/Auth.java
@@ -1,5 +1,7 @@
 package com.dteam.momentree.application.config.auth;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -8,5 +10,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Target(PARAMETER)
 @Retention(RUNTIME)
+@Parameter(hidden = true)
 public @interface Auth {
 }
