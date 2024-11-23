d025bbeb330f791351add62acccd77284d24cb91
hyungjin <hyungzin0309@naver.com>
2024-11-23 22:40:46 +0900

    cicd config

--- start_product.sh
+++ start_product.sh
@@ -1,2 +1,4 @@
 pkill -f 'java -jar'
+echo "test" > test.log
 export $(cat /home/ec2-user/.env | xargs) && nohup java -jar build/libs/momentree-0.0.1-SNAPSHOT.jar > application.log 2>&1 &
+echo "test2" > test2.log
