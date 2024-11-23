3f26ff91ccd238d1884b7d055cb5759746998f64
Karen <mingki080808@gmail.com>
2024-11-24 03:05:14 +0900

    sql 파일 제거

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO user (login_id, password) VALUES ('jimin', 'password123');
-INSERT INTO user (login_id, password) VALUES ('umc', 'password456');
-INSERT INTO user (login_id, password) VALUES ('cmc', 'adminpass');
